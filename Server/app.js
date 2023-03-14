5e3da12a38e5686f3d909fa969fc20c0f94d337b
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-13 21:05:19 -0500

    Actualizar puerto de conexion

--- Server/app.js
+++ Server/app.js
@@ -1,4 +1,4 @@
-require('./Config/Config');
+//require('./Config/Config');
 const express = require('express');
 const app = express();
 const joinPath = require('path.join');
@@ -12,9 +12,9 @@ app.use(require('./Controller/new'));*/
 
 app.use(express.static(joinPath(__dirname,'./Controller')));
 app.get('/', function (req, res) {
-    res.status(200).send({EC: 'ECUADOR', PROJECT: "ARCHIVOS", ENVIRONMENT: 'LAVACAR', VERSION: '1.0.0', BY: 'OASIS', INIT: '2023/03/13', ARCHITECT: 'ANGELS'});
+    res.status(200).send({EC: 'ECUADOR', PROJECT: "ARCHIVOS", ENVIRONMENT: 'LAVACAR', VERSION: '1.0.0', BY: 'OASIS', INIT: '2023/03/13', ARCHITECT: 'JUNIOR'});
 });
 
 app.listen(3030, () => {
-    console.log('Escuchando puerto: ', 3030);
+    console.log('Escuchando puerto: ', 3000);
 });
