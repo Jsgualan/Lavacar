68c36a383d50bec2e314da98617c67dc5deeab60
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-13 23:04:22 -0500

    Update route

--- Server/app.js
+++ Server/app.js
@@ -12,9 +12,9 @@ app.use(require('./Controller/user'));
 app.use(require('./Controller/new'));*/
 
 app.use(express.static(joinPath(__dirname,'./Controller')));
-app.get('/', async (req, res) => {
-    const querySnapshot = await db.collection('Rol').get()
-    console.log(querySnapshot);
+app.get('/', function (req, res) {
+    //const querySnapshot = await db.collection('Rol').get()
+    //console.log(querySnapshot);
     res.status(200).send({EC: 'ECUADOR', PROJECT: "ARCHIVOS", ENVIRONMENT: 'LAVACAR', VERSION: '1.0.0', BY: 'OASIS', INIT: '2023/03/13', ARCHITECT: 'JUNIOR'});
 });
 
