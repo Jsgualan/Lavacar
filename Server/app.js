f4983191fb24c731e6d63860a6027281fb99b111
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-13 23:35:49 -0500

    Update route

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,15 @@
 //require('./Config/Config');
 const express = require('express');
-const db = require('./firebase');
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+const serviceAccount = require("../firebase.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+
 const app = express();
 const joinPath = require('path.join');
 const bodyParser = require('body-parser');
