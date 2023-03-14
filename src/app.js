07f0e2b6a52406d0a32fb25041dfaa4f6111652c
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-14 16:29:15 -0500

    Update repositorie

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { FieldValue } = require('firebase-admin/firestore')
 const app = express()
 const port = 8383
-const { db } = require('./firebase')
+const { db } = require('./firebase.js')
 
 app.use(express.json())
 
