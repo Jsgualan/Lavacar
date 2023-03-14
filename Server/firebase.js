5854df1c85a0bfa77b6576b795c613cc731a3d21
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-13 23:08:29 -0500

    Update route

--- Server/firebase.js
+++ Server/firebase.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 
-const {initializeApp, applicationDefault} = require('firebase-admin/app')
-const {getFirestore} = require('firebase-admin/firestore')
+const {initializeApp, applicationDefault} = require('firebase-admin/app');
+const {getFirestore} = require('firebase-admin/firestore');
 
 initializeApp({
-    credential: applicationDefault()
-})
+    credential: applicationDefault(),
+});
 
-const db = getFirestore()
+const db = getFirestore();
 
 
 module.exports = {
