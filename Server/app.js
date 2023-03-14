02be9275b881cbf8e04af1331f528e73c4ab2de7
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-13 20:16:59 -0500

    Actualizar

---
+++ Server/app.js
@@ -0,0 +1,20 @@
+require('./Config/Config');
+const express = require('express');
+const app = express();
+const joinPath = require('path.join');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+/*app.use(require('./Controller/login'));
+app.use(require('./Controller/user'));
+app.use(require('./Controller/new'));*/
+
+app.use(express.static(joinPath(__dirname,'./Controller')));
+app.get('/', function (req, res) {
+    res.status(200).send({EC: 'ECUADOR', PROJECT: "ARCHIVOS", ENVIRONMENT: 'LAVACAR', VERSION: '1.0.0', BY: 'OASIS', INIT: '2023/03/13', ARCHITECT: 'ANGELS'});
+});
+
+app.listen(3030, () => {
+    console.log('Escuchando puerto: ', 3030);
+});
