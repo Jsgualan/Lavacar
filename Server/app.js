6268629151431ddfe72948649e0fec1afd6fd416
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-13 21:52:07 -0500

    Configuration BD

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,7 @@
 //require('./Config/Config');
 const express = require('express');
 const app = express();
+const {db} = require('.firebase')
 const joinPath = require('path.join');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
@@ -11,7 +12,9 @@ app.use(require('./Controller/user'));
 app.use(require('./Controller/new'));*/
 
 app.use(express.static(joinPath(__dirname,'./Controller')));
-app.get('/', function (req, res) {
+app.get('/', async (req, res) => {
+    const querySnapshot = await db.collection('Rol').get()
+    console.log(querySnapshot);
     res.status(200).send({EC: 'ECUADOR', PROJECT: "ARCHIVOS", ENVIRONMENT: 'LAVACAR', VERSION: '1.0.0', BY: 'OASIS', INIT: '2023/03/13', ARCHITECT: 'JUNIOR'});
 });
 
--- Server/firebase.js
+++ Server/firebase.js
@@ -1,5 +1,14 @@
 require('dotenv').config()
 
-console.log(
-    process.env.GOOGLE_APPLICATION_CREDENTIALS
-);
+const {initializeApp, applicationDefault} = require('firebase-admin/app')
+const {getFirestore} = require('firebase-admin/firestore')
+
+initializeApp({
+    credential: applicationDefault
+})
+
+const db = getFirestore()
+
+module.exports = {
+    db
+};
