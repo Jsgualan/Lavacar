5d4af81252307fc4e160e4e6587bbbaf5e5eb7c8
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-14 22:57:56 -0500

    Get user

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -3,7 +3,7 @@ const app = express();
 const { FieldValue } = require('firebase-admin/firestore')
 const { db } = require('../firebase')
 
-app.get('/login', async (req, res) => {
+app.get('/login/:type', async (req, res) => {
     const type = req.params.type;
     try {
         const userRef = db.collection('Rol').doc(type);
