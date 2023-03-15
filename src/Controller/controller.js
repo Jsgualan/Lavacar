f4215ee8054868235f1ce95ad78b72949f5edaf4
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-14 19:31:48 -0500

    Get user

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -3,6 +3,16 @@ const app = express();
 const { FieldValue } = require('firebase-admin/firestore')
 const { db } = require('../firebase')
 
+app.get('/login/:email/:password', (req, res) => {
+    const { email, password } = req.params
+    const result = db.collection('Rol').doc(req.params.email, req.params.password);
+    if (!result.exists) {
+        return res.status(200).send({ en: -1, m:'Usuario o contraseña incorrecta'});
+    }
+
+    res.status(200).send({en: 1, u: result.data()});
+})
+
 app.get('/rol', async (req, res) => {
     const peopleRef = db.collection('Rol').doc('qSyypshKJcmHeeO8R2np')
     const doc = await peopleRef.get()
