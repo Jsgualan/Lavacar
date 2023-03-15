03f8b035db600895fd8d71cae6ef112ee1eb6b3d
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-15 16:56:52 -0500

    login

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -9,6 +9,7 @@ app.get('/login/:email/:password', async (req, res) => {
         id: doc.id,
         ...doc.data()
     }))
+    console.log(user);
     if (user.length > 0) {
         return res.status(200).send({en: 1, u: user});
     }
