411c0eac42640f9f422affa30d88b4fb02dc13be
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-15 18:41:16 -0500

    List operator

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -16,6 +16,32 @@ app.get('/login/:email/:password', async (req, res) => {
     res.status(200).send({ en: -1, m:'Usuario o contraseña incorrecto '});
 })
 
+app.get('/operator', async (req, res) => {
+    const consult = await db.collection('Operator').where('state','==', true).get()
+    const response = consult.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    })) 
+    
+    if (response.length > 0) {
+        return res.status(200).send({en: 1, lP: response});
+    }
+    res.status(200).send({ en: -1, m:'No hay resultados que mostrar'});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.get('/rol', async (req, res) => {
     const querySnapshot = await db.collection("Rol").get()
     const rol = querySnapshot.docs.map(doc => ({
