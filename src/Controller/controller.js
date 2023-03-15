61399874c49fdb9e08b244240662f50fe0271b93
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-14 23:39:06 -0500

    Get rol

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -3,14 +3,13 @@ const app = express();
 const { FieldValue } = require('firebase-admin/firestore')
 const { db } = require('../firebase')
 
-app.get('/login/:type', async (req, res) => {
-    const type = req.body.type;
-    const consult = db.collection('Rol').doc(type)
-    const result = await consult.get()
-    if (!result.exists) {
-        //return res.status(200).send({ en: -1, m:'Usuario o contraseña incorrecta'});
-    }
-    //res.status(200).send({en: 1, r: result.data()});
+app.get('/login', async (req, res) => {
+    const doc = await db.collection('Rol').doc('qSyypshKJcmHeeO8R2np').get()
+
+    console.log({
+        id:doc.id,
+        ...doc.data()
+    })
 })
 
 app.get('/rol', async (req, res) => {
