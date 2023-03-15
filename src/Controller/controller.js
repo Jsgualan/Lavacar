2ca89b2cf729a2bd43c71b10316c5fcd5514dded
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-15 16:37:02 -0500

    Get user

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -5,14 +5,14 @@ const { db } = require('../firebase')
 
 app.get('/login/:email/:password', async (req, res) => {
     const querySnapshot = await db.collection('User').where('email','==', req.params.email).where('password', '==', req.params.password).get()
-    const user = querySnapshot.docs.map(doc => ({
+    /*const user = querySnapshot.docs.map(doc => ({
         id: doc.id,
         ...doc.data()
-    })) 
+    }))*/ 
     if (user.length > 0) {
-        return res.status(200).send({en: 1, u: user});
+        return res.status(200).send({en: 1, u: user.data()});
     }
-    res.status(200).send({ en: -1, m:'Usuario o contraseña incorrecto'});
+    res.status(200).send({ en: -1, m:'Usuario o contraseña incorrecto '});
 })
 
 app.get('/rol', async (req, res) => {
