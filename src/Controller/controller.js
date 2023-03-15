ed89f1402e6f4ff86e69365a435a5919cf24e5a1
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-14 19:24:06 -0500

    Get rol

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -4,10 +4,10 @@ const { FieldValue } = require('firebase-admin/firestore')
 const { db } = require('../firebase')
 
 app.get('/rol', async (req, res) => {
-    const peopleRef = db.collection('rol').doc('qSyypshKJcmHeeO8R2np')
+    const peopleRef = db.collection('Rol').doc('qSyypshKJcmHeeO8R2np')
     const doc = await peopleRef.get()
     if (!doc.exists) {
-        return res.status(400).send({ en: -1, m:'No hay datos que mostrar'});
+        return res.status(200).send({ en: -1, m:'No hay datos que mostrar'});
     }
     res.status(200).send({en: 1, r: doc.data()});
 })
