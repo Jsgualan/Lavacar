6ce38808e80cf748ebcbbbe32780f0528a91430a
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-14 23:01:41 -0500

    Get user

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -4,23 +4,21 @@ const { FieldValue } = require('firebase-admin/firestore')
 const { db } = require('../firebase')
 
 app.get('/login/:type', async (req, res) => {
-    const type = req.params.type;
-    try {
-        const userRef = db.collection('Rol').doc(type);
-        const response = await userRef.get();
-        res.send(response.data());
-      } catch(error) {
-        //res.send(error);
-      }
+    const consult = db.collection('Rol').doc(req.params.type)
+    const result = await consult.get()
+    if (!result.exists) {
+        return res.status(200).send({ en: -1, m:'Usuario o contraseña incorrecta'});
+    }
+    res.status(200).send({en: 1, r: result.data()});
 })
 
 app.get('/rol', async (req, res) => {
-    const peopleRef = db.collection('Rol').doc('qSyypshKJcmHeeO8R2np')
-    const doc = await peopleRef.get()
-    if (!doc.exists) {
+    const consult = db.collection('Rol').doc('qSyypshKJcmHeeO8R2np')
+    const result = await consult.get()
+    if (!result.exists) {
         return res.status(200).send({ en: -1, m:'No hay datos que mostrar'});
     }
-    res.status(200).send({en: 1, r: doc.data()});
+    res.status(200).send({en: 1, r: result.data()});
 })
 
 
