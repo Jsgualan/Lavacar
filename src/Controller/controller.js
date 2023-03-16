5a1eb8a0b2beec90a32cc543c60d949692edd8ac
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-15 19:14:01 -0500

    Save operator

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -34,7 +34,7 @@ app.post('/saveOperator', async (req, res) => {
     await db.collection('Operator').set({
         "name": req.params.name,
         "last_name": req.params.lastName,
-        "dni": req.password.dni,
+        "dni": req.params.dni,
         "phone": req.params.phone,
         "password": req.params.password,
         "post": req.params.post
