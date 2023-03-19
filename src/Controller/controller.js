9a216cf96272014ac432ce08704edcb464696b40
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-19 04:06:25 -0500

    get hour

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -43,7 +43,7 @@ app.get('/checkOperator/:dni', async (req, res) => {
 })
 
 app.get('/hourDay/:id', async (req, res) => {
-    const consult = await db.collection('Schedule').where('id','==', req.params.id).where("state","==",true).get()
+    const consult = await db.collection('Schedule').where('id','==', req.params.id).where('state','==',true).get()
     const response = consult.docs.map(doc => ({
         id: doc.id,
         ...doc.data()
