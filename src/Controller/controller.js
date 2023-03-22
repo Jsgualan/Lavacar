59b0699baf45697768bfe8f5aa3385dc7880e161
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-03-22 00:05:18 -0500

    get notification

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -97,9 +97,18 @@ app.post('/saveReserve', async (req, res) => {
             
 })
 
-
-
-
+app.get('/getNotification/:date', async (req, res) => {
+    const consult = await db.collection('Reserve').where('dateReserve','==', req.params.date).where('state', '==', 1).get()
+    const response = consult.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    })) 
+    
+    if (response.length > 0) {
+        return res.status(200).send({en: -1, lN: response});
+    }
+    res.status(200).send({ en: 1, m:'Operador permitido'});
+})
 
 
 
