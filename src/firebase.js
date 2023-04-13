ca4920a6f7faf4b87a64233e0115363ec0e42ff0
OASIS-NEST <global-angelgalvez2789@gmail.com>
2023-04-13 09:17:06 -0500

    send notification

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
-/*const { initializeApp, cert } = require('firebase-admin/app')
+const { initializeApp, cert } = require('firebase-admin/app')
 const { getFirestore } = require('firebase-admin/firestore')
-const admin = require('firebase-admin')
 
 const serviceAccount = require('../firebase.json')
 
@@ -12,7 +11,7 @@ const db = getFirestore()
 
 module.exports = { db }
 
-const message = {
+/*const message = {
     tokens: ["daiN_WAcS8KkIb2HLm73yY:APA91bEGE6FYtKTMV_wunmOHbIgqe5giLBWI4jYXpZxfRVf2bs5D-HxiZw-3FYyZCH1H2wkEkEqjQzA2poqMhJNIT0GBf7ro3cz1d7gtGkHq4-7TfDgQuVLKPVQdIdYzFZq3dRu3UbhG"],
     notification: {
       title: "Nueva reserva",
@@ -25,16 +24,3 @@ function message(message){
 
     });
 }*/
-
-const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-
-const serviceAccount = require('../firebase.json');
-
-initializeApp({
-    credential: cert(serviceAccount)
-  });
-
-const db = getFirestore();
-
-module.exports = { db }
